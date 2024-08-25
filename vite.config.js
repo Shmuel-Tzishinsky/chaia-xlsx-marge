cd3d2261b9381fbe6bf249a366e6ac55dce615fe
Your Name <a0527188257@gmail.com>
2024-08-26 01:11:13 +0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+
+  base: "/haia-xlsx-marge/", // הוסף כאן את שם המאגר שלך
+});
